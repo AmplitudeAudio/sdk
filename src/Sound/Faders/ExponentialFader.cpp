8ea44712e184fbdad3d5840744138a5891640f01
Axel Nana <axel.nana@aliens-group.com>
2021-09-02 13:04:06 +0100

    🐛 Fix bugs and crashes with the `ExponentialFader`

--- src/Sound/Faders/ExponentialFader.cpp
+++ src/Sound/Faders/ExponentialFader.cpp
@@ -16,8 +16,22 @@
 
 namespace SparkyStudios::Audio::Amplitude
 {
+    /**
+     * @brief Offset used to avoid NaN results.
+     */
+    const float kOffset = 0.5f;
+
     float ExponentialFader::GetFromPercentage(double percentage)
     {
-        return m_from * AM_PowerF(m_to / m_from, (float)percentage);
+        percentage = AM_CLAMP(percentage, 0.0, 1.0);
+
+        float from = m_from + kOffset;
+        float to = m_to + kOffset;
+        float factor = percentage == 0.0 ? 1 : AM_PowerF(to / from, (float)percentage);
+
+        float lower = AM_MIN(m_from, m_to);
+        float greater = AM_MAX(m_from, m_to);
+
+        return AM_CLAMP(from * factor - kOffset, lower, greater);
     }
 } // namespace SparkyStudios::Audio::Amplitude
