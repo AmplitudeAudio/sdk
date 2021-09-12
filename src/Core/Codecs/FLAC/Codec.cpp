8ddbe9354a425f4b03411122d06051efbd8e9496
Axel Nana <axel.nana@aliens-group.com>
2021-09-12 22:05:26 +0100

    feat(codec): :sparkles: Add FLAC codec

---
+++ src/Core/Codecs/FLAC/Codec.cpp
@@ -0,0 +1,150 @@
+// Copyright (c) 2021-present Sparky Studios. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#define DR_FLAC_IMPLEMENTATION
+#include "dr_flac.h"
+
+#include <Core/Codecs/FLAC/Codec.h>
+
+namespace SparkyStudios::Audio::Amplitude::Codecs
+{
+    bool FLACCodec::FLACDecoder::Open(AmOsString filePath)
+    {
+        if (!m_codec->CanHandleFile(filePath))
+        {
+            CallLogFunc("The FLAC codec cannot handle the file: '%s'\n", filePath);
+            return false;
+        }
+
+#if defined(AM_WCHAR_SUPPORTED)
+        _flac = drflac_open_file_w(filePath, nullptr);
+#else
+        _flac = drflac_open_file(filePath, nullptr);
+#endif
+        if (_flac == nullptr)
+        {
+            CallLogFunc("Cannot load the FLAC file: '%s'\n.", filePath);
+            return false;
+        }
+
+        m_format.SetAll(
+            _flac->sampleRate, _flac->channels, _flac->bitsPerSample, _flac->totalPCMFrameCount, _flac->channels * sizeof(float),
+            AM_SAMPLE_FORMAT_FLOAT, // This codec always read frames as float32 values
+            AM_SAMPLE_INTERLEAVED // dr_flac always read interleaved frames
+        );
+
+        _initialized = true;
+
+        return true;
+    }
+
+    bool FLACCodec::FLACDecoder::Close()
+    {
+        if (_initialized)
+        {
+            m_format = SoundFormat();
+            _initialized = false;
+            delete _flac;
+            _flac = nullptr;
+        }
+
+        // true because it is already closed
+        return true;
+    }
+
+    AmUInt64 FLACCodec::FLACDecoder::Load(AmReal32Buffer out)
+    {
+        if (!_initialized)
+        {
+            return 0;
+        }
+
+        if (drflac_seek_to_pcm_frame(_flac, 0) != DRFLAC_TRUE)
+        {
+            return 0;
+        }
+
+        return drflac_read_pcm_frames_f32(_flac, _flac->totalPCMFrameCount, out);
+    }
+
+    AmUInt64 FLACCodec::FLACDecoder::Stream(AmReal32Buffer out, AmUInt64 offset, AmUInt64 length)
+    {
+        if (!_initialized)
+        {
+            return 0;
+        }
+
+        if (!Seek(offset))
+        {
+            return 0;
+        }
+
+        return drflac_read_pcm_frames_f32(_flac, length, out);
+    }
+
+    bool FLACCodec::FLACDecoder::Seek(AmUInt64 offset)
+    {
+        return drflac_seek_to_pcm_frame(_flac, offset) == DRFLAC_TRUE;
+    }
+
+    bool FLACCodec::FLACEncoder::Open(AmOsString filePath)
+    {
+        _initialized = true;
+        return false;
+    }
+
+    bool FLACCodec::FLACEncoder::Close()
+    {
+        if (_initialized)
+        {
+            return true;
+        }
+
+        return true;
+    }
+
+    AmUInt64 FLACCodec::FLACEncoder::Write(const float* in, AmUInt64 offset, AmUInt64 length)
+    {
+        return 0;
+    }
+
+    Codec::Decoder* FLACCodec::CreateDecoder() const
+    {
+        return new FLACDecoder(this);
+    }
+
+    Codec::Encoder* FLACCodec::CreateEncoder() const
+    {
+        return new FLACEncoder(this);
+    }
+
+    bool FLACCodec::CanHandleFile(AmOsString filePath) const
+    {
+        // TODO: Maybe check by extension instead?
+
+#if defined(AM_WCHAR_SUPPORTED)
+        drflac* flac = drflac_open_file_w(filePath, nullptr);
+#else
+        drflac* flac = drflac_open_file(filePath, nullptr);
+#endif
+
+        if (flac != nullptr)
+        {
+            delete flac;
+            return true;
+        }
+
+        return false;
+    }
+} // namespace SparkyStudios::Audio::Amplitude::Codecs
---
+++ src/Core/Codecs/FLAC/Codec.h
@@ -0,0 +1,87 @@
+// Copyright (c) 2021-present Sparky Studios. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#ifndef SS_AMPLITUDE_AUDIO_FLAC_CODEC_H
+#define SS_AMPLITUDE_AUDIO_FLAC_CODEC_H
+
+#include <SparkyStudios/Audio/Amplitude/Amplitude.h>
+
+#include "dr_flac.h"
+
+namespace SparkyStudios::Audio::Amplitude::Codecs
+{
+    [[maybe_unused]] static class FLACCodec final : public Codec
+    {
+    public:
+        class FLACDecoder final : public Codec::Decoder
+        {
+        public:
+            explicit FLACDecoder(const Codec* codec)
+                : Codec::Decoder(codec)
+                , _initialized(false)
+                , _flac()
+            {}
+
+            bool Open(AmOsString filePath) final;
+
+            bool Close() final;
+
+            AmUInt64 Load(AmReal32Buffer out) final;
+
+            AmUInt64 Stream(AmReal32Buffer out, AmUInt64 offset, AmUInt64 length) final;
+
+            bool Seek(AmUInt64 offset) final;
+
+        private:
+            bool _initialized;
+            drflac* _flac;
+        };
+
+        class FLACEncoder final : public Codec::Encoder
+        {
+        public:
+            explicit FLACEncoder(const Codec* codec)
+                : Codec::Encoder(codec)
+                , _initialized(false)
+                , _flac()
+            {}
+
+            bool Open(AmOsString filePath) final;
+
+            bool Close() final;
+
+            AmUInt64 Write(const float* in, AmUInt64 offset, AmUInt64 length) final;
+
+        private:
+            bool _initialized;
+            drflac* _flac;
+        };
+
+        FLACCodec()
+            : Codec("flac")
+        {}
+
+        ~FLACCodec() final = default;
+
+        [[nodiscard]] Decoder* CreateDecoder() const final;
+
+        [[nodiscard]] Encoder* CreateEncoder() const final;
+
+        bool CanHandleFile(AmOsString filePath) const final;
+    } flac_codec; // NOLINT(cert-err58-cpp)
+} // namespace SparkyStudios::Audio::Amplitude::Codecs
+
+#endif // SS_AMPLITUDE_AUDIO_FLAC_CODEC_H
