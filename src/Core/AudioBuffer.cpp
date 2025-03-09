302926fe5f378d9422e97780b6db1ded6af2f1b6
Axel Nana <axel.nana@aliens-group.com>
2025-03-09 01:18:03 +0100

    fix(core): Reset channel state on destruct. Clear only active channels.

--- src/Core/AudioBuffer.cpp
+++ src/Core/AudioBuffer.cpp
@@ -18,6 +18,13 @@
 
 namespace SparkyStudios::Audio::Amplitude
 {
+    AudioBufferChannel::~AudioBufferChannel()
+    {
+        _begin = nullptr;
+        _frameCount = 0;
+        _isEnabled = false;
+    }
+
     AmSize AudioBufferChannel::size() const
     {
         return _frameCount;
@@ -237,7 +244,8 @@ namespace SparkyStudios::Audio::Amplitude
     void AudioBuffer::Clear()
     {
         for (auto& channel : _channels)
-            channel.clear();
+            if (channel.enabled())
+                channel.clear();
     }
 
     const AmAlignedReal32Buffer& AudioBuffer::GetData() const
